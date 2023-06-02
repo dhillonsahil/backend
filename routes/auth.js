14adb5b909735aa52d7fd0d5f276558ebea1cd0e
Sahil <sahil2742002@gmail.com>
2023-06-02 22:27:52 +0530

    login end point created

--- routes/auth.js
+++ routes/auth.js
@@ -52,4 +52,40 @@ router.post('/createuser', [
    }
 })
 
+// User login using POST : "api/auth/login" No authenticatiomn(auth token) required
+router.post('/login', [
+    // validating correct details entered or not
+    body('email','Please enter a valid email').isEmail(),
+    body('password','Password cannot be blank').exists()
+], async (req, res) => {// if not added correctly errors
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array()});
+    }
+
+    const {email, password} = req.body;
+    try {
+        let user =await User.findOne({email})
+        if(!user){
+            return res.status(400).json({error : "Please check Your Login Credentials"})
+        }
+
+        const passwordCompare =await bcrypt.compare(password,user.password)
+        if(!passwordCompare){
+            return res.status(400).json({error : "Please check Your Login Credentials"})
+        }
+
+        const data={
+            user:{
+                id:user.id
+            }
+        }
+        const authToken = jwt.sign(data,jwt_secret)
+        res.json(authToken)
+    } catch (error) {
+        console.error("internal server error");
+        return res.status(500).json({error : "Internal Server error"})
+    }
+})
+
 module.exports = router;

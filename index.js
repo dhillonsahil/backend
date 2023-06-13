7d7c2f5f7dfa9684030147597053453785b3cb99
Sahil <60694942+dhillonsahil@users.noreply.github.com>
2023-06-13 08:19:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const connectToMongo = require('./db')
 const express = require('express')
 connectToMongo();
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 6010;
 
 app.use(express.json()) // to use req.body we have to use this
 app.use('/api/auth',require('./routes/auth'))
